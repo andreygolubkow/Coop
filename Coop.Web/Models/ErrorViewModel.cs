4ae68aeb67da9e4c4c51e247ae68cd595a0fc4a3
AndreyGolubkow <AndreyGolubkow@gmail.com>
2021-07-26 23:13:10 +0700

    Добавил файлы проекта

---
+++ Coop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Coop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
