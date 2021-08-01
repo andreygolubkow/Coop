b5afa0bb4ada77da815bd21ae26fd5f7350b8cbb
AndreyGolubkow <AndreyGolubkow@gmail.com>
2021-08-01 16:19:34 +0700

    Добавлены страницы AspNetIdentity

---
+++ Coop.Web/Controllers/RealtyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Coop.Web.Controllers
+{
+    public class RealtyController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
